92a053d32e96b438ad9d8ab2416fb9d0c45e844d
Ismael de Sousa <ismaeldesousa2014@outlook.com>
2022-06-08 15:12:12 -0300

    Create Hiring-Plan-report.ts

---
+++ Hiring-Plan-report.ts
@@ -0,0 +1,79 @@
+function main(workbook: ExcelScript.Workbook) {
+	/**
+	 * Hiring Plan Report
+	 */
+	const HIRING_PLAN_REPORT_SHEET = workbook.getWorksheet('hiring_plan_report_2022-01-01_2');
+
+	const createJobTable = () => {
+		if (workbook.getTable('Job_Informations_Table')) {
+			throw new Error('A table already exists. Delete it, paste your informations, and run the script again')
+		} else if (!HIRING_PLAN_REPORT_SHEET.getUsedRange()) {
+			throw new Error('There is no data in the spreadsheet. Clean it completely, paste your information and run the script again')
+		}
+
+		HIRING_PLAN_REPORT_SHEET.getAutoFilter().remove()
+		// Add a new table at extended range obtained by extending right, then down from range A1:Z1 on HIRING_PLAN_REPORT_SHEET
+		workbook.addTable(HIRING_PLAN_REPORT_SHEET.getRange('A1:Z1').getExtendedRange(ExcelScript.KeyboardDirection.down), true).setName('Job_Informations_Table')
+
+		createHiringManagerInfosTable()
+	}
+
+	const createHiringManagerInfosTable = () => {
+		const HIRING_MANAGER_TABLE_HEADERS = [
+			'BU',
+			'Director',
+			'Opened Date',
+			'Closing Date',
+			'gap to fill',
+			'Complexity',
+			'TECH or not',
+			'Region'
+		]
+
+		let sheetRowLength = HIRING_PLAN_REPORT_SHEET.getUsedRange().getLastColumn().getColumnIndex()
+		let hiringManagerRange = HIRING_PLAN_REPORT_SHEET.getRangeByIndexes(0, sheetRowLength + 1, 1, HIRING_MANAGER_TABLE_HEADERS.length)
+
+		for (let i = 0; i < HIRING_MANAGER_TABLE_HEADERS.length; i++) {
+			HIRING_PLAN_REPORT_SHEET.getCell(0, sheetRowLength + i + 1).setValue(HIRING_MANAGER_TABLE_HEADERS[i])
+		}
+		
+		let jobTableInformations: ExcelScript.Table = workbook.getTable('Hiring_Manager_Table')
+
+		setFormula(jobTableInformations)
+	}
+
+	const setFormula = (table: ExcelScript.Table) => {
+		let buColumn = table.getColumnByName('BU')
+		let directorColumn = table.getColumnByName('Director')
+		let openedDateColumn = table.getColumnByName('Opened date')
+
+		// add formula into BU, Director and Opened Date column cells
+		buColumn.getRangeBetweenHeaderAndTotal().setFormulaLocal("=PROCV(K2;'To. For'!A:B;2;0)")
+		directorColumn.getRangeBetweenHeaderAndTotal().setFormulaLocal("=PROCV(K2;'To. For'!A:C;3;0)")
+		openedDateColumn.getRangeBetweenHeaderAndTotal().setFormulaLocal("=ESQUERDA(S2;7)")
+
+		newPivotTable()
+	}
+
+
+
+	/**
+	 * Pivotable
+	 */
+	const PIVOT_TABLE_SHEET = workbook.getWorksheet('pivot');
+	const HIRING_PLAN_REPORT_TABLE = workbook.getTable("Job_Informations_Table");
+
+	const newPivotTable = () => {
+		let newPivotTable = workbook.getPivotTable('PivotTable_hiringPlan')
+
+		workbook.addPivotTable('PivotTable_hiringPlan', HIRING_PLAN_REPORT_TABLE, PIVOT_TABLE_SHEET.getCell(3, 0))
+	}
+
+	createJobTable()
+
+	/**
+	 * Suport messaging
+	 */
+	console.log('If you need support, email me at: ismael.moura@sinch.com or send a message in Microsoft Teams to: Ismael de Sousa Paulino Moura')
+
+}
